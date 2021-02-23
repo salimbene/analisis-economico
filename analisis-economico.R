eaa8fa04724cfca94732b424995fef4b9b64133e
Matias Salimbene <msalimbe@ar.ibm.com>
2021-02-23 16:07:52 -0300

    kick off

---
+++ analisis-economico.R
@@ -0,0 +1,22 @@
+
+
+# pre-work jubilaciones mínimas desde 1971
+jubilaciones <- read.csv("/home/rstudio/project-objectstorage/jubilacion-minima-desde-1971.csv")
+
+#ver structura
+str(jubilaciones)
+
+#ver stats
+summary(jubilaciones)
+
+#renombrar columnas
+colnames(jubilaciones) <- c("Fecha","Pesos")
+
+#converir columna a tipo fecha
+jubilaciones$Fecha <- as.Date(jubilaciones$Fecha)
+
+
+head(jubilaciones)
+
+
+?read.csv
