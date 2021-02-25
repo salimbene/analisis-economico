f4bba1cb52513aeb26d713cf0fb2de6352a97414
Matias Salimbene <msalimbe@ar.ibm.com>
2021-02-25 14:44:56 +0000

    trabajando con salario y jubilaciones - ver formato de fechas

--- analisis-economico.R
+++ analisis-economico.R
@@ -1,6 +1,7 @@
 
 
 # pre-work jubilaciones mínimas desde 1971
+#source datos.gob.ar
 jubilaciones <- read.csv("/home/rstudio/project-objectstorage/jubilacion-minima-desde-1971.csv")
 
 #ver structura
@@ -15,7 +16,31 @@ colnames(jubilaciones) <- c("Fecha","Pesos")
 #converir columna a tipo fecha
 jubilaciones$Fecha <- as.Date(jubilaciones$Fecha)
 
+head(jubilaciones, 5)
+tail(jubilaciones, 5)
 
-head(jubilaciones, 10)
+#pre-work salario minimo desde 1988
+#source datos.gob.ar
+salario <- read.csv("/home/rstudio/project-objectstorage/indice-salario-minimo-vital-movil-valores-mensuales-pesos-corrientes-desde-1988.csv")
 
-tail(jubilaciones,5)
+#ver estructura
+str(salario)
+
+#ver stats
+summary(salario)
+
+#renombrar columnas
+colnames(salario) <- c("Fecha","Mensual","Diario","Hora")
+
+#converir columna a tipo fecha
+salario$Fecha <- as.Date(salario$Fecha)
+
+#lala <- salario$Pesos * 21
+
+
+head(salario, 5)
+tail(salario, 10)
+
+salario[salario$Fecha == "01-01-95", ]
+jubilaciones[jubilaciones$Fecha == "1995-01-01", ]
+
