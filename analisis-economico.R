7af7dc3b0f57969aefd63f14833a229d3432efb1
Matias Salimbene <msalimbe@ar.ibm.com>
2021-02-26 19:15:23 +0000

    dates

--- analisis-economico.R
+++ analisis-economico.R
@@ -21,7 +21,7 @@ tail(jubilaciones, 5)
 
 #pre-work salario minimo desde 1988
 #source datos.gob.ar
-salario <- read.csv("/home/rstudio/project-objectstorage/indice-salario-minimo-vital-movil-valores-mensuales-pesos-corrientes-desde-1988.csv")
+salario <- read.csv("/home/rstudio/project-objectstorage/indice-salario-minimo-vital-movil-valores-mensuales-pesos-corrientes-desde-1988.csv", stringsAsFactors = F)
 
 #ver estructura
 str(salario)
@@ -33,14 +33,36 @@ summary(salario)
 colnames(salario) <- c("Fecha","Mensual","Diario","Hora")
 
 #converir columna a tipo fecha
-salario$Fecha <- as.Date(salario$Fecha)
+salario$Fecha <- as.Date(salario$Fecha) #format = "%d-%m-%Y"
 
 #lala <- salario$Pesos * 21
+?as.Date
+?strptime
+?as.difftime
+
+tail(jubilaciones)
+head(salario)
+
+jubilaciones[600, 1] == salario[672, 1]
 
 
-head(salario, 5)
-tail(salario, 10)
+salario[salario$Fecha == "01-01-95", 1]
+jubilaciones[jubilaciones$Fecha == "1995-01-01", 1]
 
-salario[salario$Fecha == "01-01-95", ]
-jubilaciones[jubilaciones$Fecha == "1995-01-01", ]
+nrow(salario[salario$Fecha > "2022-01-01", ])
+head(salario)
+
+salario[salario$Fecha > "2022-01-01", 1] <- salario[salario$Fecha > "2022-01-01", 1] - as.difftime(365, unit="days")
+
+
+
+substr(a,1,2)
+
+a <- "123456"
+a(0)
+
+
+
+
+
 
